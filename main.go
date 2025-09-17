2de5ea76921bd79c6594cf717d646bc8d764c4f9
Christopher Lamm <christopherlamm81@icloud.com>
2025-09-17 10:52:31 -0400

    extract file writing to function

--- main.go
+++ main.go
@@ -62,29 +62,38 @@ func main() {
 	}
 
 	for _, doc := range cache.State.Documents {
-		// TODO: extract to function and test
-		contents := doc.Title + "\n" + doc.NotesMarkdown
-
-		safeTitle, err := filenamify.Filenamify(doc.Title, filenamify.Options{})
+		err := writeDocument(doc)
 		if err != nil {
-			fmt.Printf("error creating safe filename: %v", err)
-			os.Exit(1)
-		}
-
-		err = os.MkdirAll("output", 0755)
-		if err != nil {
-			fmt.Printf("error creating output directory: %v", err)
-			os.Exit(1)
-		}
-
-		filename := fmt.Sprintf("%s-%s.md", safeTitle, doc.ID)
-		if err := os.WriteFile("output/"+filename, []byte(contents), 0644); err != nil {
-			fmt.Printf("error writing file %s: %v", filename, err)
+			fmt.Printf("error writing document %s: %v", doc.ID, err)
 			os.Exit(1)
 		}
 	}
 
 	os.Exit(0)
+}
+
+func writeDocument(doc Document) error {
+	contents := doc.Title + "\n" + doc.NotesMarkdown
+
+	safeTitle, err := filenamify.Filenamify(doc.Title, filenamify.Options{})
+	if err != nil {
+		fmt.Printf("error creating safe filename: %v", err)
+		os.Exit(1)
+	}
+
+	err = os.MkdirAll("output", 0755)
+	if err != nil {
+		fmt.Printf("error creating output directory: %v", err)
+		os.Exit(1)
+	}
+
+	filename := fmt.Sprintf("%s-%s.md", safeTitle, doc.ID)
+	if err := os.WriteFile("output/"+filename, []byte(contents), 0644); err != nil {
+		fmt.Printf("error writing file %s: %v", filename, err)
+		os.Exit(1)
+	}
+
+	return nil
 }
 
 // createCache takes a byte slice of JSON data and unmarshals it into a Cache struct.
